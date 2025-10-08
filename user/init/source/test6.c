89f7e30623bd7844af4759470cb061d3778b84d9
iProgramInCpp <iprogramincpp@gmail.com>
2025-10-08 15:06:11 +0300

    * Init: Make test #6 print a different pattern

--- user/init/source/test6.c
+++ user/init/source/test6.c
@@ -55,12 +55,25 @@ void RunTest6()
 		return;
 	}
 
+	// Draw a recognizable pattern
 	uint32_t RowStart = 0;
 	
-	uint32_t Thr1 = Info.Height * 1 / 5;
-	uint32_t Thr2 = Info.Height * 2 / 5;
-	uint32_t Thr3 = Info.Height * 3 / 5;
-	uint32_t Thr4 = Info.Height * 4 / 5;
+	uint32_t Thr[7];
+	for (int i = 0; i < 7; i++) {
+		Thr[i] = Info.Width * (i + 1) / 7;
+	}
+	uint32_t Thr2[6];
+	for (int i = 0; i < 6; i++) {
+		Thr2[i] = Info.Width * (i + 1) / 6;
+	}
+
+	uint32_t ThrV = Info.Height * 2 / 3;
+	uint32_t ThrV2 = Info.Height * 3 / 4;
+	
+	const uint32_t BarColors1[] = { 0xc0c0c0, 0xc0c000, 0x00c0c0, 0x00c000, 0xc000c0, 0xc00000, 0x0000c0 };
+	const uint32_t BarColors2[] = { 0x0000c0, 0x131313, 0xc000c0, 0x131313, 0x00c0c0, 0x131313, 0xc0c0c0 };
+	const uint32_t BarColors3[] = { 0x00214c, 0xffffff, 0x32006a, 0x131313, 0xffffff, 0x131313 };
+	const uint32_t BarColors4[] = { 0x090909, 0x131313, 0x1d1d1d };
 	
 	for (uint32_t i = 0; i < Info.Height; i++)
 	{
@@ -69,10 +82,44 @@ void RunTest6()
 		for (uint32_t j = 0; j < Info.Width; j++)
 		{
 			uint32_t Color = 0xFFFFFF;
-			if (i < Thr1 || i > Thr4)
-				Color = 0x00FFFF;
-			else if (i < Thr2 || i > Thr3)
-				Color = 0xDF9AFF;
+			
+			if (i < ThrV)
+			{
+				for (int k = 0; k < 7; k++) {
+					if (j < Thr[k]) {
+						Color = BarColors1[k];
+						break;
+					}
+				}
+			}
+			else if (i < ThrV2)
+			{
+				for (int k = 0; k < 7; k++) {
+					if (j < Thr[k]) {
+						Color = BarColors2[k];
+						break;
+					}
+				}
+			}
+			else {
+				for (int k = 0; k < 6; k++) {
+					if (j < Thr2[k]) {
+						Color = BarColors3[k];
+						if (k == 4) {
+							uint32_t diff1 = j - Thr2[k - 1];
+							uint32_t diff2 = Thr2[k] - Thr2[k - 1];
+							
+							if (diff1 < diff2 * 1 / 3)
+								Color = BarColors4[0];
+							else if (diff1 < diff2 * 2 / 3)
+								Color = BarColors4[1];
+							else
+								Color = BarColors4[2];
+						}
+						break;
+					}
+				}
+			}
 			
 			Row[j] = Color;
 		}
