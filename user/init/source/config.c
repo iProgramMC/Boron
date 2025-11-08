9eb171536b684cb75bcdb399d8b471ebe14942c2
iProgramInCpp <iprogramincpp@gmail.com>
2025-11-09 01:18:44 +0200

    * Init: Begin reading config file.

--- user/init/source/config.c
+++ user/init/source/config.c
@@ -20,6 +20,33 @@ BSTATUS LoadConfigFile(const char* File)
 		return Status;
 	}
 	
+	uint64_t FileLength = 0;
+	Status = OSGetLengthFile(Handle, &FileLength);
+	if (FAILED(Status))
+	{
+		DbgPrint("Init: Failed to get size of config file '%s'. %s", File, RtlGetStatusString(Status));
+		OSClose(Handle);
+		return Status;
+	}
+	
+	char* Data = OSAllocate(FileLength);
+	if (!Data)
+	{
+		DbgPrint("Init: Failed to allocate space to read config file.");
+		OSClose(Handle);
+		return STATUS_INSUFFICIENT_MEMORY;
+	}
+	
+	IO_STATUS_BLOCK Iosb;
+	Status = OSReadFile(&Iosb, Handle, 0, Data, FileLength, 0);
+	if (FAILED(Status) || Iosb.BytesRead != FileLength)
+	{
+		DbgPrint("Init: Failed to read config file '%s' (read %zu bytes). %s", File, Iosb.BytesRead, RtlGetStatusString(Status));
+		OSClose(Handle);
+		return Status;
+	}
+	
 	OSClose(Handle);
+	
 	return STATUS_SUCCESS;
 }
