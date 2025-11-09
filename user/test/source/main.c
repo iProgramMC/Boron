538202a3efeae444f3c97bee510e48e877369dbc
iProgramInCpp <iprogramincpp@gmail.com>
2025-11-10 00:28:29 +0200

    * Test: Add line reading test.
    This tests the new "finish on newline" feature of the pipe, as well
    as the terminal's line discipline.

--- user/test/source/main.c
+++ user/test/source/main.c
@@ -1,15 +1,21 @@
 #include <boron.h>
 
-extern int TestFunction(); // libtest.so
-
 int _start()
 {
-	DbgPrint("Hello from test.exe!");
-	
-	int i = 0;
+	PPEB Peb = OSGetCurrentPeb();
 	while (true)
 	{
-		OSPrintf("Hello, world!  Number %d.\n", ++i);
-		OSSleep(1000);
+		OSPrintf("Type something in: ");
+		
+		char Buffer[512];
+		IO_STATUS_BLOCK Iosb;
+		BSTATUS Status = OSReadFile(&Iosb, Peb->StandardInput, 0, Buffer, sizeof Buffer, IO_RW_FINISH_ON_NEWLINE);
+		if (FAILED(Status))
+		{
+			OSPrintf("FAILED to read: %s\n", RtlGetStatusString(Status));
+			OSSleep(2000);
+		}
+		
+		OSPrintf("You typed in: '%s'.\n", Buffer);
 	}
 }
