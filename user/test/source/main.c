4c44a36fbead408029394147baf24884f289cdfd
iProgramInCpp <iprogramincpp@gmail.com>
2025-11-10 00:52:40 +0200

    * FullScreenTerminal: Rename to VideoTerminal.

--- user/test/source/main.c
+++ user/test/source/main.c
@@ -1,3 +1,4 @@
+// 10 November 2025 - iProgramInCpp
 #include <boron.h>
 
 int _start()
@@ -7,15 +8,17 @@ int _start()
 	{
 		OSPrintf("Type something in: ");
 		
-		char Buffer[512];
+		char Buffer[500];
 		IO_STATUS_BLOCK Iosb;
-		BSTATUS Status = OSReadFile(&Iosb, Peb->StandardInput, 0, Buffer, sizeof Buffer, IO_RW_FINISH_ON_NEWLINE);
+		BSTATUS Status = OSReadFile(&Iosb, Peb->StandardInput, 0, Buffer, sizeof Buffer - 1, IO_RW_FINISH_ON_NEWLINE);
 		if (FAILED(Status))
 		{
 			OSPrintf("FAILED to read: %s\n", RtlGetStatusString(Status));
 			OSSleep(2000);
 		}
 		
+		Buffer[sizeof Buffer - 1] = 0;
+		Buffer[Iosb.BytesRead] = 0;
 		OSPrintf("You typed in: '%s'.\n", Buffer);
 	}
 }
