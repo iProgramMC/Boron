5cc38763ef81a8275e095c2786a4ee6b36289b1f
iProgramInCpp <iprogramincpp@gmail.com>
2025-11-25 00:28:29 +0200

    * Mm: Add support for overwriting memory mappings.
    This code is untested, but I am curious to see how much there is to
    change afterwards.  This is an extremely brittle function which I'm
    most likely going to get wrong the first try.

--- boron/source/mm/vad.c
+++ boron/source/mm/vad.c
@@ -108,6 +108,232 @@ BSTATUS MiInitializeAndInsertVad(
 	return STATUS_SUCCESS;
 }
 
+// This is probably the most pain I've ever felt writing a memory manager function.
+// Seriously. This is edge case highway right here.
+//
+// (Just you wait until the Page Table Duplicator(TM) 3000(R)...)
+BSTATUS MmOverrideAddressRange(PEPROCESS Process, uintptr_t StartAddress, size_t SizePages, PMMADDRESS_NODE* OutAddrNode)
+{
+	PMMVAD TempVad1 = MmAllocatePool(POOL_NONPAGED, Process->Heap.ItemSize);
+	PMMVAD TempVad2 = MmAllocatePool(POOL_NONPAGED, Process->Heap.ItemSize);
+	PMMVAD TempVad3 = MmAllocatePool(POOL_NONPAGED, Process->Heap.ItemSize);
+	
+	if (!TempVad1 || !TempVad2 || !TempVad3)
+	{
+		DbgPrint("MmOverrideAddressRange: Failed to allocate temporary VADs.");
+		if (TempVad1) MmFreePool(TempVad1);
+		if (TempVad2) MmFreePool(TempVad2);
+		if (TempVad3) MmFreePool(TempVad3);
+		return STATUS_INSUFFICIENT_MEMORY;
+	}
+	
+	memset(TempVad1, 0, Process->Heap.ItemSize);
+	memset(TempVad2, 0, Process->Heap.ItemSize);
+	memset(TempVad3, 0, Process->Heap.ItemSize);
+	
+	// As earlier me said (in mm/pt.h), we need to acquire the address space lock first
+	// to avoid a lock ordering bug.  Rwlocks DO support recursive ownership in exclusive
+	// mode, so just do that.
+	KIPL Ipl = MmLockSpaceExclusive(0);
+	
+	// Then lock the VAD list's lock.  It also supports recursive locking, thankfully.
+	PMMVAD_LIST VadList = MmLockVadListProcess(Process);
+	
+	uintptr_t EndAddress = StartAddress + SizePages * PAGE_SIZE;
+	
+	// Unmap and/or shrink each VAD inside this range.
+	for (PRBTREE_ENTRY VadTreeEntry = GetFirstEntryRbTree(&VadList->Tree);
+		VadTreeEntry != NULL;
+		VadTreeEntry = GetNextEntryRbTree(VadTreeEntry))
+	{
+		PMMVAD Vad = CONTAINING_RECORD(VadTreeEntry, MMVAD, Node.Entry);
+		
+		// Check if there is any overlap.
+		if (EndAddress <= Vad->Node.StartVa || Node_EndVa(&Vad->Node) <= StartAddress)
+			continue;
+		
+		// There is some overlap.  Is it a complete overlap?
+		if (StartAddress <= Vad->Node.StartVa && Node_EndVa(&Vad->Node) <= EndAddress)
+		{
+			// Complete overlap, so the whole VAD will be unmapped.
+			//
+			// Lock the VAD list again because MiReleaseVad will acquire it.
+			MiLockVadList(VadList);
+			MiReleaseVad(Vad);
+			continue;
+		}
+		
+		// There is partial overlap.
+		
+		// The worst case scenario is that the range is entirely engulfed on both sides
+		// by the same VAD, in which case we will need to split it up.
+		if (Vad->Node.StartVa < StartAddress && EndAddress < Node_EndVa(&Vad->Node))
+		{
+			// Clean split into two.
+			TempVad2->Node.StartVa = EndAddress;
+			TempVad2->Node.Size = (Node_EndVa(&Vad->Node) - EndAddress) / PAGE_SIZE;
+			
+			// Copy properties from the VAD.
+			TempVad2->Flags = Vad->Flags;
+			TempVad2->Mapped.Object = Vad->Mapped.Object;
+			
+			if (Vad->Mapped.Object)
+			{
+				ObReferenceObjectByPointer(Vad->Mapped.Object);
+			
+				// Calculate a new section offset.
+				TempVad2->SectionOffset = Vad->SectionOffset + (EndAddress - Vad->Node.StartVa);
+			}
+			
+			// TODO: Temporarily set this VAD's Committed flag to false, so that
+			// MiDecommitVad won't generate new page tables for the decommitted region.
+			bool WasCommitted = Vad->Flags.Committed;
+			Vad->Flags.Committed = false;
+			
+			// Lock the VAD list again because MiDecommitVad will unlock it.
+			//
+			// NOTE: Already own the address space rwlock, so no need to worry about lock
+			// ordering issues here.
+			MiLockVadList(VadList);
+			
+			// Decommit the VAD at this offset.  Note that this function call will
+			// release the lock we just acquired above.
+			//
+			// Also note that normally this would create page tables for the region,
+			// which we don't want. (we'd be very hard-pressed to fail right now!)
+			// So we do the trick of turning off the committed flag while decommitting.
+			// Note that any extant page faults will be waiting on either the VAD lock
+			// or the address space lock, both of which we own, so they won't see this
+			// intermediate state.
+			MiDecommitVad(VadList, Vad, StartAddress, SizePages);
+			
+			// Then shrink this VAD.
+			Vad->Node.Size = (StartAddress - Vad->Node.StartVa) / PAGE_SIZE;
+			
+			// Finally, restore the committed flag.
+			Vad->Flags.Committed = WasCommitted;
+			
+			// Now we're done with the original VAD. But we still need to add the second
+			// one to the tree. So do it now.
+			InsertItemRbTree(&VadList->Tree, &TempVad2->Node.Entry);
+			
+			// Set TempVad2 to NULL so it won't be freed when this function exits.
+			TempVad2 = NULL;
+			
+			// And also break, since we're pretty sure no more VADs will overlap
+			// this range.
+			break;
+		}
+		
+		if (Vad->Node.StartVa < StartAddress && Node_EndVa(&Vad->Node) <= EndAddress)
+		{
+			// Overlap on the left side.
+			
+			// This is a common theme. You'll see this in the right hand overlap case too.
+			//
+			// See the above case (complete overlap requiring a split) for an explanation
+			// of the locking and decommit scheme.
+			bool WasCommitted = Vad->Flags.Committed;
+			Vad->Flags.Committed = false;
+			
+			// Decommit the specified region.
+			MiLockVadList(VadList);
+			MiDecommitVad(VadList, Vad, StartAddress, (Node_EndVa(&Vad->Node) - StartAddress) / PAGE_SIZE);
+			
+			// Resize the VAD.
+			Vad->Node.Size = (StartAddress - Vad->Node.StartVa) / PAGE_SIZE;
+			
+			Vad->Flags.Committed = WasCommitted;
+			continue;
+		}
+		
+		if (StartAddress <= Vad->Node.StartVa && EndAddress < Node_EndVa(&Vad->Node))
+		{
+			// Overlap on the right size.
+			
+			bool WasCommitted = Vad->Flags.Committed;
+			Vad->Flags.Committed = false;
+			
+			size_t Offset = (EndAddress - Vad->Node.StartVa);
+			
+			// Decommit the specified region.
+			MiLockVadList(VadList);
+			MiDecommitVad(VadList, Vad, Vad->Node.StartVa, Offset / PAGE_SIZE);
+			
+			Vad->Node.StartVa += Offset;
+			Vad->Node.Size -= Offset / PAGE_SIZE;
+			if (Vad->Mapped.Object)
+				Vad->SectionOffset += Offset;
+			
+			Vad->Flags.Committed = WasCommitted;
+			continue;
+		}
+		
+		KeCrash("MmOverrideAddressRange: Unhandled case (1)");
+	}
+	
+	// Remove or shrink each item in the heap.
+	for (PRBTREE_ENTRY HeapTreeEntry = GetFirstEntryRbTree(&Process->Heap.Tree);
+		HeapTreeEntry != NULL;
+		HeapTreeEntry = GetNextEntryRbTree(HeapTreeEntry))
+	{
+		// Note that I'm not simply using MmAllocateAddressRange because this causes
+		// additional allocations which I'm not too fond of doing.
+		
+		PMMADDRESS_NODE Node = CONTAINING_RECORD(HeapTreeEntry, MMADDRESS_NODE, Entry);
+		
+		// Check if there is any overlap.
+		if (EndAddress <= Node->StartVa || Node_EndVa(Node) <= StartAddress)
+			continue;
+		
+		if (Node->StartVa < StartAddress && EndAddress < Node_EndVa(Node))
+		{
+			// Clean split into two.
+			// Thankfully we don't have to do any shenanigans with decommitting.
+			TempVad3->Node.StartVa = EndAddress;
+			TempVad3->Node.Size = (Node_EndVa(Node) - EndAddress) / PAGE_SIZE;
+			Node->Size = (StartAddress - Node->StartVa) / PAGE_SIZE;
+			InsertItemRbTree(&Process->Heap.Tree, &TempVad3->Node.Entry);
+			
+			// Set TempVad3 to NULL so it won't be freed after its insertion into the tree.
+			TempVad3 = NULL;
+			continue;
+		}
+		
+		if (Node->StartVa < StartAddress && Node_EndVa(Node) <= EndAddress)
+		{
+			// Overlap on the left side.
+			Node->Size = (StartAddress - Node->StartVa) / PAGE_SIZE;
+			continue;
+		}
+		
+		if (StartAddress <= Node->StartVa && EndAddress < Node_EndVa(Node))
+		{
+			// Overlap on the right side.
+			size_t Offset = (EndAddress - Node->StartVa);
+			Node->StartVa += Offset;
+			Node->Size -= Offset / PAGE_SIZE;
+			continue;
+		}
+		
+		KeCrash("MmOverrideAddressRange: Unhandled case (2)");
+	}
+	
+	// Set up the address node with the new range.
+	TempVad1->Node.StartVa = StartAddress;
+	TempVad1->Node.Size = SizePages;
+	
+	*OutAddrNode = &TempVad1->Node;
+	
+	MmUnlockVadList(VadList);
+	MmUnlockSpace(Ipl, 0);
+	
+	if (TempVad2) MmFreePool(TempVad2);
+	if (TempVad3) MmFreePool(TempVad3);
+	
+	return STATUS_SUCCESS;
+}
+
 // NOTE: This does not unlock the VAD list.
 BSTATUS MmReserveVirtualMemoryVad(size_t SizePages, int AllocationType, int Protection, void* StartAddress, PMMVAD* OutVad, PMMVAD_LIST* OutVadList)
 {
@@ -116,6 +342,8 @@ BSTATUS MmReserveVirtualMemoryVad(size_t SizePages, int AllocationType, int Prot
 	
 	BSTATUS Status;
 
+	if ((AllocationType & (MEM_FIXED | MEM_OVERRIDE)) == (MEM_FIXED | MEM_OVERRIDE))
+		Status = MmOverrideAddressRange(Process, (uintptr_t) StartAddress, SizePages, &AddrNode);
 	if (AllocationType & MEM_FIXED)
 		Status = MmAllocateAddressRange(&Process->Heap, (uintptr_t) StartAddress, SizePages, &AddrNode);
 	else
