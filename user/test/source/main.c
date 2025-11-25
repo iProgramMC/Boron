43ea55ff7eecc13f9222ba4a69db36b1ce309b5e
iProgramInCpp <iprogramincpp@gmail.com>
2025-11-25 10:32:01 +0200

    * Test: Add some more testing code.

--- user/test/source/main.c
+++ user/test/source/main.c
@@ -1,5 +1,6 @@
 // 10 November 2025 - iProgramInCpp
 #include <boron.h>
+#include <string.h>
 
 #define CHECK_FAILURE() do { \
 	if (FAILED(Status)) {    \
@@ -58,6 +59,35 @@ int _start()
 	);
 	CHECK_FAILURE();
 	DbgPrint("Right side overlap succeeded. BaseAddress3: %p", BaseAddress4);
+	
+	// map the entire center.
+	void* BaseAddress5 = (uint8_t*)BaseAddress3 + RegionSize3;
+	size_t RegionSize5 = RegionSize2 - RegionSize3 - RegionSize4;
+	Status = OSAllocateVirtualMemory(
+		CURRENT_PROCESS_HANDLE,
+		&BaseAddress5,
+		&RegionSize5,
+		MEM_COMMIT | MEM_RESERVE | MEM_FIXED | MEM_OVERRIDE,
+		PAGE_READ | PAGE_WRITE
+	);
+	CHECK_FAILURE();
+	DbgPrint("Full overlap succeeded. BaseAddress5: %p", BaseAddress5);
+	
+	// how about remapping the entire region PLUS some free space?
+	void* BaseAddress6 = BaseAddress;
+	size_t RegionSize6 = RegionSize1 + 512 * 1024;
+	Status = OSAllocateVirtualMemory(
+		CURRENT_PROCESS_HANDLE,
+		&BaseAddress5,
+		&RegionSize5,
+		MEM_COMMIT | MEM_RESERVE | MEM_FIXED | MEM_OVERRIDE,
+		PAGE_READ | PAGE_WRITE
+	);
+	CHECK_FAILURE();
+	DbgPrint("Full overlap + free space succeeded. BaseAddress5: %p", BaseAddress5);
+	
+	memset(BaseAddress6, 0, RegionSize6);
+	DbgPrint("Memset of the entire region succeeded. BaseAddress3: %p", BaseAddress4);
 	
 	PPEB Peb = OSGetCurrentPeb();
 	while (true)
