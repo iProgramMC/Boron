c709a5fdcfc728b3a5f29ef23f47c41120026252
iProgramInCpp <iprogramincpp@gmail.com>
2025-12-31 12:05:52 +0200

    * Make a fat disk image instead of an ISO

--- tools/build_image_armv6.mk
+++ tools/build_image_armv6.mk
@@ -2,9 +2,11 @@
 
 $(IMAGE_TARGET): kernel drivers apps initrd
 	@echo "[MK]\tBuilding iso..."
-	@rm -rf $(ISO_DIR)
-	@mkdir -p $(ISO_DIR)
-	@cp $(KERNEL_ELF) $(ISO_DIR)/$(KERNEL_NAME)
-	@cp -r $(BUILD_DIR)/*.sys $(BUILD_DIR)/*.tar $(ISO_DIR)
-	@xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table --protective-msdos-label $(ISO_DIR) -o $@ 2>/dev/null
-	@rm -rf $(ISO_DIR)
+	rm -rf $(ISO_DIR)
+	mkdir -p $(ISO_DIR)
+	cp $(KERNEL_ELF) $(ISO_DIR)/$(KERNEL_NAME)
+	cp -r $(BUILD_DIR)/*.sys $(BUILD_DIR)/*.tar $(ISO_DIR)	
+	dd if=/dev/zero of=$(IMAGE_TARGET) bs=1M count=64
+	mkfs.vfat $(IMAGE_TARGET)
+	mcopy -i $(IMAGE_TARGET) -s $(ISO_DIR)/* ::
+
