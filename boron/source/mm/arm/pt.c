67934128339d7a40f8b7f4220e7bdff757af918d
iProgramInCpp <iprogramincpp@gmail.com>
2026-03-16 17:19:45 +0200

    * Mm/Arm: Fix wrong mapping of exception handler table
    Due to happenstance it happened to map to 0x8000000 (because 0x108000 << 12 = 0x08000000)
    so I was really confused when I saw OpeniBoot code being executed...

--- boron/source/mm/arm/pt.c
+++ boron/source/mm/arm/pt.c
@@ -85,7 +85,7 @@ HPAGEMAP MiCreatePageMapping()
 	// The 1008th entry of Jibbie maps the exception handler pointers
 	// (at 0xFFFF0000), so map it too.  Note that KiExceptionHandlerTable
 	// is a physical address.
-	JibbiePtr[1008] = MmBuildPte((uintptr_t)KiExceptionHandlerTable, MM_PROT_READ | MM_PROT_WRITE | MM_PROT_EXEC);
+	JibbiePtr[1008] = MmBuildPte(MmPhysPageToPFN((uintptr_t) KiExceptionHandlerTable), MM_PROT_READ | MM_PROT_WRITE | MM_PROT_EXEC);
 	
 	PMMPTE RootPtr = MmGetHHDMOffsetAddr(MmPFNToPhysPage(NewPageMapping));
 	PMMPTE OldRootPtr = (PMMPTE) MI_PML1_LOCATION;
@@ -119,6 +119,10 @@ HPAGEMAP MiCreatePageMapping()
 	MmEndUsingHHDM();
 	MmFlushTlbUpdates();
 	MmUnlockKernelSpace();
+	
+	DbgPrint("ROOT: %p", NewPageMapping*PAGE_SIZE);
+	DbgPrint("Jibbie: %p", Jibbie*PAGE_SIZE);
+	DbgPrint("Debbie: %p", Debbie*PAGE_SIZE);
 	
 	uintptr_t NewPageMappingResult = MmPFNToPhysPage (NewPageMapping);
 	return (HPAGEMAP) NewPageMappingResult;
