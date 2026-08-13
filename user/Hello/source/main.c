6eebf8c597181c62284bcf7d3bf65f71ff6dba4f
iProgramInCpp <iprogramincpp@gmail.com>
2026-08-13 21:12:10 +0300

    * Hello: Add
    I need a simple application that does nothing crazy in order to debug
    memory leaks.

---
+++ user/Hello/source/main.c
@@ -0,0 +1,12 @@
+// 13 August 2026 - iProgramInCpp
+#include <boron.h>
+#include <string.h>
+
+int main(int ArgumentCount, char** ArgumentArray)
+{
+	(void) ArgumentCount;
+	(void) ArgumentArray;
+	
+	OSPrintf("Hello, world!\n");
+	return 0;
+}
